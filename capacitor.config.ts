05fd3133d9bdfe7098647b3d6987df33ed27718d
Jonahvan47 <petpassportai@gmail.com>
2026-04-13 19:40:29 -0700

    Add Android platform and privacy policy

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mytrialguard.app',
+  appName: 'TrialGuard',
+  webDir: 'dist'
+};
+
+export default config;
